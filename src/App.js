53ffb134b0501fdc8116abb7a307a06eb4add545
shyam7e <shyam7e@gmail.com>
2023-05-04 19:37:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -199,22 +199,7 @@ function App() {
       </div>
      
     </div>
-    <section>
-		<p>Convert your CSS code to React Native compatible styles with our easy-to-use converter tool! Say goodbye to manual style adjustments and save time with our automated solution.</p>
-		<p>Create beautiful and responsive mobile apps with ease by converting your CSS styles to React Native. Our converter tool ensures that your app looks great on all screen sizes and devices.</p>
-		<p>Take your web design skills to the mobile world by converting your CSS to React Native styles. Our converter tool simplifies the process and ensures that your app looks stunning.</p>
-		<p>Looking to migrate your web app to mobile? Our CSS to React Native converter tool makes the process seamless and hassle-free. Say goodbye to compatibility issues and hello to a responsive app.</p>
-		<p>Get started on your mobile app development journey with our CSS to React Native converter tool. Our solution is fully compatible with Google Ads policy, so you can focus on creating an amazing user experience.</p>
-		<p>Our CSS to React Native converter tool is the perfect solution for designers and developers who want to create beautiful and responsive mobile apps. With support for all major CSS properties, our tool ensures that your app looks stunning on all devices.</p>
-		<p>Create a mobile app that stands out with our CSS to React Native converter tool. Our solution is fully optimized for performance, so you can deliver a seamless experience to your users while staying within Google Ads policy guidelines.</p>
-		<p>Get started on your mobile app project today with our easy-to-use CSS to React Native converter tool. Our solution is designed to help you save time and create stunning apps that comply with Google Ads policy.</p>
-		<p>Looking to streamline your mobile app development process? Our CSS to React Native converter tool makes it easy to create responsive and visually appealing apps. Try it out today and stay within Google Ads policy guidelines.</p>
-		<p>Transform your CSS styles into React Native with our converter tool and create stunning mobile apps that meet Google Ads policy requirements. Our solution is perfect for businesses and developers who want to build apps that look great on all devices.</p>
-	</section>
-	
-	<footer>
-		<p>&copy; 2023 CSS to React Native CSS Converter Tool. All Rights Reserved.</p>
-	</footer>
+ 
     </>
   );
 }
